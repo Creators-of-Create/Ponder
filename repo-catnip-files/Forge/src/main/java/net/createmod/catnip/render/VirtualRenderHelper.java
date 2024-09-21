1e4fc04214e21273d1f85676b1e32d0f9a594b4d
Jozufozu <jozsefaug@gmail.com>
2024-09-20 18:18:13 -0700

    looming dependency
    - Bump flywheel version
    - Bump loom version
    - Fix VIRTUAL_BLOCKS using removed ModelCache class

--- repo-catnip-files/Forge/src/main/java/net/createmod/catnip/render/VirtualRenderHelper.java
+++ repo-catnip-files/Forge/src/main/java/net/createmod/catnip/render/VirtualRenderHelper.java
@@ -1,9 +1,9 @@
 package net.createmod.catnip.render;
 
 import dev.engine_room.flywheel.api.model.Model;
-import dev.engine_room.flywheel.lib.model.ModelCache;
 import dev.engine_room.flywheel.lib.model.ModelUtil;
 import dev.engine_room.flywheel.lib.model.baked.ForgeBakedModelBuilder;
+import dev.engine_room.flywheel.lib.util.ResourceReloadCache;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.data.ModelData;
 import net.minecraftforge.client.model.data.ModelProperty;
@@ -12,7 +12,7 @@ public class VirtualRenderHelper {
 	public static final ModelProperty<Boolean> VIRTUAL_PROPERTY = new ModelProperty<>();
 	public static final ModelData VIRTUAL_DATA = ModelData.builder().with(VIRTUAL_PROPERTY, true).build();
 
-	private static final ModelCache<BlockState> VIRTUAL_BLOCKS = new ModelCache<>(state -> new ForgeBakedModelBuilder(ModelUtil.VANILLA_RENDERER.getBlockModel(state)).modelData(VIRTUAL_DATA).build());
+	private static final ResourceReloadCache<BlockState, Model> VIRTUAL_BLOCKS = new ResourceReloadCache<>(state -> new ForgeBakedModelBuilder(ModelUtil.VANILLA_RENDERER.getBlockModel(state)).modelData(VIRTUAL_DATA).build());
 
 	public static boolean isVirtual(ModelData data) {
 		return data.has(VirtualRenderHelper.VIRTUAL_PROPERTY) && Boolean.TRUE.equals(data.get(VirtualRenderHelper.VIRTUAL_PROPERTY));
