a0e04ab60d4dadc4df7775a9ad708eb9813a8fc9
FodziCz <soukup.filip@volny.cz>
2025-03-17 14:51:06 +0100

    Changed the Glob.toRegexPattern function so that it now supports negative lookahead groups (Syntax: "{!word1,word2,word3}")

--- Common/src/main/java/net/createmod/catnip/data/Glob.java
+++ Common/src/main/java/net/createmod/catnip/data/Glob.java
@@ -117,7 +117,14 @@ public class Glob {
 						throw new PatternSyntaxException("Cannot nest groups", globPattern, i - 1);
 					}
 
-					regex.append("(?:");
+					regex.append("(?");
+					if (next(globPattern, i) == '!') {
+						regex.append('!');
+					} else {
+						regex.append(":");
+					}
+					++i;
+
 					inGroup = true;
 				}
 				case '}' -> {
