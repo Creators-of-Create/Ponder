05f56134cca88ad041e38779fcc12d6cf6ff0ddf
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2025-03-25 21:48:42 +0800

    Remove commands while preserving the styles.

--- Common/src/main/java/net/createmod/catnip/nbt/NBTProcessors.java
+++ Common/src/main/java/net/createmod/catnip/nbt/NBTProcessors.java
@@ -2,7 +2,6 @@ package net.createmod.catnip.nbt;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.function.UnaryOperator;
 
@@ -13,6 +12,7 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.StringTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
@@ -42,24 +42,39 @@ public final class NBTProcessors {
 	private static final UnaryOperator<CompoundTag> signProcessor = data -> {
 		var front_text = data.getCompound("front_text").getList("messages", Tag.TAG_STRING);
 		var back_text = data.getCompound("back_text").getList("messages", Tag.TAG_STRING);
-		for (int i = 0; i < 4; ++i) {
-			tryRemovingCommand(front_text, i);
-			tryRemovingCommand(back_text, i);
-		}
+		ListTag front_text2 = removeCommands(front_text);
+		ListTag back_text2 = removeCommands(back_text);
+		data.getCompound("front_text").put("messages",front_text2);
+		data.getCompound("back_text").put("messages",back_text2);
 		return data;
 	};
 
-	private static void tryRemovingCommand(ListTag front_text, int i) {
-		if(NBTProcessors.textComponentHasClickEvent(front_text.get(i).getAsString()))
+	private static ListTag removeCommands(ListTag inList) {
+		ListTag result = new ListTag();
+		inList.stream()
+			.map(t->
+				{
+					var component = Component.Serializer.fromJson(t.getAsString());
+					if(component != null) {
+						return StringTag.valueOf(Component.Serializer.toJson(
+							removeCommand(component)
+						));
+					}
+					return StringTag.valueOf("");
+				}
+			)
+			.forEach(result::add);
+		return result;
+	}
+
+	private static MutableComponent removeCommand(MutableComponent textComponent){
+		textComponent.setStyle(textComponent.getStyle().withClickEvent(null));
+		for(Component component : textComponent.getSiblings())
 		{
-			var text =
-				Component.Serializer.fromJson(front_text.get(i).getAsString());
-			if (text != null) {
-				text.setStyle(text.getStyle().withClickEvent(null));
-				front_text.remove(i);
-				front_text.add(i,net.minecraft.nbt.StringTag.valueOf(Component.Serializer.toJson(text)));
-			}
+			if(component instanceof MutableComponent textComponent2)
+				removeCommand(textComponent2);
 		}
+		return textComponent;
 	}
 	public static UnaryOperator<CompoundTag> itemProcessor(String tagKey) {
 		return data -> {
